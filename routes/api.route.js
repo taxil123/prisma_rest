2c9b17c902b6eecd5c16a1b910b6e97f1cc9c9d4
taxil123 <64030805+taxil123@users.noreply.github.com>
2022-02-24 20:30:10 +0530

    first commit

---
+++ routes/api.route.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+router.get('/products', async (req, res, next) => {
+  res.send({ message: 'Ok api is working 🚀' });
+});
+
+router.get('/products/:id', async (req, res, next) => {
+  res.send({ message: 'Ok api is working 🚀' });
+});
+
+router.get('/products/:id', async (req, res, next) => {
+  res.send({ message: 'Ok api is working 🚀' });
+});
+
+router.get('/products/:id', async (req, res, next) => {
+  res.send({ message: 'Ok api is working 🚀' });
+});
+
+module.exports = router;
